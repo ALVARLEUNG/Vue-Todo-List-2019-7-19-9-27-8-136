44abcad3a51f9014a12d8548caabf1e20625d8d0
ALVARLEUNG <alvar.leung@qq.com>
2019-07-21 19:03:40 +0800

    [LST]feat: add showItem for TodoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,23 +7,26 @@
       </p>
     </div>
     <div>
-      <el-input v-model="inputText" class="input-text" placeholder="请输入内容" ></el-input>
+      <el-input v-model="inputText" class="input-text" placeholder="请输入内容"></el-input>
       <el-button id="button" @click="addItem" @keyup.enter="addItem">Add</el-button>
     </div>
     <br>
-    <div  v-for="(item,index) in itemList">
-      {{index+1}}<el-checkbox :checked="item.status === 'complete'" @change="check(index)"><span v-if="item.status !== 'complete'">{{item.text}}</span><del v-else>{{item.text}}</del></el-checkbox>
+    <div v-for="(item,index) in itemList">
+      {{index+1}}
+      <el-checkbox :checked="item.status === 'complete'" @change="check(index)"><span v-if="item.status !== 'complete'">{{item.text}}</span>
+        <del v-else>{{item.text}}</del>
+      </el-checkbox>
     </div>
     <div>
       <ul id="filters">
         <li>
-          <a href="#" data-filter="all">ALL</a>
+          <a href="#" @click="showItem('All')" data-filter="all">ALL</a>
         </li>
         <li>
-          <a href="#" data-filter="active">Active</a>
+          <a href="#" @click="showItem('Active')" data-filter="active">Active</a>
         </li>
         <li>
-          <a href="#" data-filter="complete">Complete</a>
+          <a href="#" @click="showItem('Complete')" data-filter="complete">Complete</a>
         </li>
       </ul>
     </div>
@@ -34,23 +37,44 @@
   export default {
     name: "TodoList",
     data() {
-    return {
-      itemList: [],
-      inputText: '',
-    };
-  },
+      return {
+        allItems: [],
+        completeItems: [],
+        activeItems: [],
+        itemList: [],
+        inputText: '',
+        status: 'All'
+      };
+    },
     methods: {
-      addItem () {
+      addItem() {
         if (this.inputText !== '') {
-          let item ={text:this.inputText, status: 'active'};
-          this.itemList.push(item);
+          let item = {text: this.inputText, status: 'active'};
+          this.allItems.push(item);
           this.inputText = '';
+          this.showItem(this.status);
         }
       },
-      check (index) {
-        this.itemList[index].status === 'active'?
-          this.itemList[index].status = 'complete':  this.itemList[index].status = 'active'
-
+      check(index) {
+        this.allItems[index].status === 'active' ?
+          this.allItems[index].status = 'complete' : this.allItems[index].status = 'active';
+        this.itemList = this.allItems;
+      },
+      showItem (status) {
+        switch (status) {
+          case 'All' :
+            this.status = 'All';
+            this.itemList = this.allItems;
+            break;
+          case 'Active':
+            this.status = 'Active';
+            this.itemList = this.allItems.filter(item => item.status === 'active');
+            break;
+          case 'Complete':
+            this.status = 'Complete';
+            this.itemList = this.allItems.filter(item => item.status === 'complete');
+            break;
+        }
 
       }
     }
